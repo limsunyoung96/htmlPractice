1f975fa65f3fe1fd75d579018e844c194e3b7e6d
SunyoungLim <gsw0981@naver.com>
2020-08-03 17:38:58 +0900

    0803html

--- 볼품없는게시판/jsp.js
+++ 볼품없는게시판/jsp.js
@@ -1,6 +1,13 @@
 // 아래 내용은 서버에서 동작하는 것이 아니고 브라우져(클라이언트)에서 동작합니다.
 // JSP의 메소드를 클라이언트쪽에서 구현해 본것입니다.
 
+/* 
+    만약 %27
+        디코딩 전, 이 상태에서 split("=")하면 원하는대로 문자열이 잘리는데
+            aaa.html?content=dsks%27dlfa 
+        디코딩 후, 이 상태에서 split("=")하면 내용이 중간에 잘려버림
+            aaa.html?content=dsks=dlfa 
+    */
 var request = {}; // 빈객체 생성
 request.getParameter = function (p_name) {    // 메소드 추가
     var v_url = location.href;
@@ -19,14 +26,16 @@ request.getParameter = function (p_name) {    // 메소드 추가
 request.getParameterValues = function (p_name) {
     var v_url = location.href;
     if (v_url.indexOf("?") == -1) return;
-    v_url = decodeURIComponent(v_url);  // 디코딩
+    // 디코딩을 너무 빨리해서 넘어온 값 속에 =, 이 섞여있으면
+    // split에서 문제가 된다.
+    // v_url = decodeURIComponent(v_url);  // 디코딩
     var v_retArr = [];   // 값을 담을 빈배열 생성
     var v_queryString = v_url.split("?")[1];
     var v_nameValues = v_queryString.split("&");
     for (var i = 0; i < v_nameValues.length; i++) {
         var v_nameValue = v_nameValues[i].split("=");
-        if (v_nameValue[0] == p_name) {
-            v_retArr[v_retArr.length] = v_nameValue[1];
+        if (decodeURIComponent(v_nameValue[0]) == p_name) {
+            v_retArr[v_retArr.length] = decodeURIComponent(v_nameValue[1]);
         }
     }
     if (v_retArr.length == 0) {
