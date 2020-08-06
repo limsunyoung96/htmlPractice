808475f62e93a1007107755ed35e5eabfe184c43
SunyoungLim <gsw0981@naver.com>
2020-08-06 17:11:59 +0900

    0806html

--- AJAX/0805check.php
+++ AJAX/0805check.php
@@ -1,3 +1,13 @@
 <?php
-    echo $_POST["name"];
+    $response = "";
+    $response = $response.$_POST["nm_name"];
+    $response = $response.$_POST["nm_al1"];
+    $response = $response.$_POST["nm_al2"];
+    $response = $response.$_POST["nm_al3"];
+    $response = $response.$_POST["nm_al4"];
+    $response = $response.$_POST["nm_al5"];
+    $response = $response.$_POST["nm_al6"];
+    $response = $response.$_POST["nm_al7"];
+    $response = $response.$_POST["nm_al8"];
+    echo $response;
 ?>
