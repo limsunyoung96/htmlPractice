ea3d47d277fab3935e593e6c990fc48af3eeeb40
SunyoungLim <gsw0981@naver.com>
2020-08-12 13:58:28 +0900

    0812html

---
+++ AJAX/0812check2.php
@@ -0,0 +1,14 @@
+<?php
+    $response = "";
+    $response = $response.$_GET["nm_name"];
+    $response = $response.$_GET["nm_al1"];
+    $response = $response.$_GET["nm_al2"];
+    $response = $response.$_GET["nm_al3"];
+    $response = $response.$_GET["nm_al4"];
+    $response = $response.$_GET["nm_al5"];
+    $response = $response.$_GET["nm_al6"];
+    $response = $response.$_GET["nm_al7"];
+    $response = $response.$_GET["nm_al8"];
+    echo $response;
+?>
+<!-- 0812check2.php -->
